1028209f8fbe9829898354ee032b947b238a18dd
Ziva Xu <yxxuziva@gmail.com>
2018-07-23 16:22:32 +0800

    Finished working website

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -10,12 +10,13 @@ class GamesController < ApplicationController
 
   def score
     @grid = LETTERS
+    @grid_m = @grid
     @answer = params[:answer]
     user_content = JSON.parse(open("https://wagon-dictionary.herokuapp.com/#{@answer}").read)
-    contains = @answer.upcase.split("").all? { |letter| @grid.delete(letter) }
+    contains = @answer.upcase.split("").all? { |letter| @grid_m.delete(letter) }
     if !contains
       @message = "Sorry but #{@answer} can't be built out of #{@grid.join(', ')}"
-    elsif user_content['found']
+    elsif !user_content['found']
       @message = "Sorry but #{@answer} does not seem to be a valid English word..."
     else
       @message = "Congratulations! #{@answer} is a valid English word!"
