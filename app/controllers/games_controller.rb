9de9a39067753fdddfc97d1950741a46593881b3
Ziva Xu <yxxuziva@gmail.com>
2018-07-23 15:53:09 +0800

    Still needs testing

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,25 @@
+require 'json'
+require 'open-uri'
+
+class GamesController < ApplicationController
+  LETTERS = [*('A'..'Z')].sample(10)
+
+  def new
+    @grid = LETTERS
+  end
+
+  def score
+    @grid = LETTERS
+    @answer = params[:answer]
+    user_content = JSON.parse(open("https://wagon-dictionary.herokuapp.com/#{@answer}").read)
+    contains = @answer.upcase.split("").all? { |letter| @grid.delete(letter) }
+    if !contains
+      @message = "Sorry but #{@answer} can't be built out of #{@grid.join(', ')}"
+    elsif user_content['found']
+      @message = "Sorry but #{@answer} does not seem to be a valid English word..."
+    else
+      @message = "Congratulations! #{@answer} is a valid English word!"
+    end
+  end
+end
+
